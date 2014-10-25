993a1cfcf958d8385e447c6a955e15b2e6977302
Henrik Fogelberg <henrik.fogelberg@afconsult.com>
2014-10-25 09:07:47 +0200

    Contact scaffold removed

--- app/views/contacts/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @contact, :id, :first_name, :last_name, :email, :phone, :message, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -38,17 +38,6 @@ ActiveRecord::Schema.define(version: 20141022074017) do
     t.datetime "updated_at"
   end
 
-  create_table "contacts", force: true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "phone"
-    t.text     "message"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "artwork_id"
-  end
-
   create_table "illustrations", force: true do |t|
     t.string   "name"
     t.string   "image"
