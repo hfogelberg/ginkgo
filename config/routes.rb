9c99503b35f866b08cbdde6f2d2f07716b7beefc
Henrik Fogelberg <henrik.fogelberg@afconsult.com>
2014-10-26 20:48:18 +0100

    Disable signup for Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Ginkgo::Application.routes.draw do
 
   devise_for :users
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
+
   resources :artworks
 
   resources :collections
